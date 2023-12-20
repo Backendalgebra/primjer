c949879a64b2f56f021b24b80189169d6ea388fc
Michael <bors996@gmail.com>
2023-12-20 20:13:08 +0100

    varijable

---
+++ osnove_php/primjer_inlude.php
@@ -0,0 +1,6 @@
+<?php
+require "../osnove_html/forma-ispis.php";
+include "../osnove_html/forma-ispis.php";
+echo "neki text";
+
+?>
---
+++ osnove_php/varijable.php
@@ -0,0 +1,12 @@
+<?php
+$ime="ante";
+$Ime="Mate";
+
+echo $ime." ".$Ime;
+
+echo $jkjdklldaj="jsaksla";
+
+define("PI",3.14);
+echo PI;
+$radius=5;
+echo "opseg kruga je ".$radius*2*PI;

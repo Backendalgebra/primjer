54c73a16d9a0cafa17f7c77bc020b86cca5cf780
Michael <bors996@gmail.com>
2024-01-15 14:13:43 +0100

    zaostatak

--- osnove_php/datoteka_citanje.php
+++ osnove_php/datoteka_citanje.php
@@ -1,9 +1,19 @@
 <?php
+header("content-type:application/json");
 //print_r($GLOBALS);
 $globalsJSON=json_encode($GLOBALS,JSON_PRETTY_PRINT);
-echo $globalsJSON;
+//echo $globalsJSON;
 
 $file="globalne_varijable.json";
 $curent=@file_get_contents($file);
 $curent .= "john smith\n";
 file_put_contents($file,$globalsJSON);
+
+//citaj file
+
+$procitanifile=file_get_contents($file,false,null,0,filesize($file));
+//echo $procitanifile;
+
+$dekodiraniniz=json_decode($procitanifile,1);
+//print_r($dekodiraniniz);
+echo $dekodiraniniz["_SERVER"]["Path"];
